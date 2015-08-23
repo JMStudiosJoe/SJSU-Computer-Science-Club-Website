b4e2ec476c36c159297adbd4463e62964f0900f2
JMSDev <jrichardson@jmstudios.net>
2015-08-22 22:00:21 -0700

    Base functionality completed. Needs formatting and CSS design.

--- src/js/navDiv.js
+++ src/js/navDiv.js
@@ -12,18 +12,20 @@ var IconExample = React.createClass({
 	}
 });
 var contentPositioning = {
-	 "padding-top" : "15%",
-	 "padding-left" : "10%",
-	 backgroundImage : "url('../img/EDITBackground.jpg')"
+	 "padding-top" : "3%",
+	 "padding-left" : "40%",
+	 backgroundImage : "url('../src/img/EDITBackground.jpg')",
+	 "background-position-x": "-10%",
+	 "background-repeat": "no-repeat"
 };
 //backgroundImage: 'url(' + imgUrl + ')'
 var postDisplayStyle = {
-	maxWidth : 450,
+	maxWidth : 400,
 	border : 6,
 	padding :60,
 	margin : 60,
 	backgroundColor: 'white',
-	"box-shadow" : "6px 6px 6px"
+	"box-shadow" : "6px 6px 36px"
 };
 var navBarStyling = {
 	marginLeft: 10,
@@ -66,38 +68,96 @@ var NavigationDiv = React.createClass({
 	},
 	showHomeTab:function()
 	{
-		this.resetDivs();
-		this.setState({ showHome: !this.state.showHome });
+		
+		
+		this.setState({ showHome: true,
+			showAbout: false,
+			showJoin: false,
+			showTutoring: false,
+			showJobs: false,
+			showTips: false,
+			showProjects: false
+		});
+		
 	},
 	showAboutTab:function()
 	{
-		this.resetDivs();
-		this.setState({ showAbout: !this.state.showAbout });
+		//this.setState({ showAbout: !this.state.showAbout });
+		
+		this.setState({ showHome: false,
+			showAbout: true,
+			showJoin: false,
+			showTutoring: false,
+			showJobs: false,
+			showTips: false,
+			showProjects: false
+		});
 	},
 	showJoinTab:function()
 	{
-		this.resetDivs();
-		this.setState({ showJoin: !this.state.showJoin });
+		
+		//this.setState({ showJoin: !this.state.showJoin });
+		//this.resetDivs();
+		this.setState({ showHome: false,
+			showAbout: false,
+			showJoin: true,
+			showTutoring: false,
+			showJobs: false,
+			showTips: false,
+			showProjects: false
+		});
 	},
 	showTutoringTab:function()
 	{
-		this.resetDivs();
-		this.setState({ showTutoring: !this.state.showTutoring });
+		
+		//this.setState({ showTutoring: !this.state.showTutoring });
+		this.setState({ showHome: false,
+			showAbout: false,
+			showJoin: false,
+			showTutoring: true,
+			showJobs: false,
+			showTips: false,
+			showProjects: false
+		});
 	},
 	showJobsTab:function()
 	{
-		this.resetDivs();
-		this.setState({ showJobs: !this.state.showJobs });
+		
+		// this.setState({ showJobs: !this.state.showJobs });
+		this.setState({ showHome: false,
+			showAbout: false,
+			showJoin: false,
+			showTutoring: false,
+			showJobs: true,
+			showTips: false,
+			showProjects: false
+		});
 	},
 	showTipsTab:function()
 	{
-		this.resetDivs();
-		this.setState({ showTips: !this.state.showTips });
+		
+		//this.setState({ showTips: !this.state.showTips });
+		this.setState({ showHome: false,
+			showAbout: false,
+			showJoin: false,
+			showTutoring: false,
+			showJobs: false,
+			showTips: true,
+			showProjects: false
+		});
 	},
 	showProjectsTab:function()
 	{
-		this.resetDivs();
-		this.setState({ showProjects: !this.state.showProjects });
+		
+		// this.setState({ showProjects: !this.state.showProjects });
+		this.setState({ showHome: false,
+			showAbout: false,
+			showJoin: false,
+			showTutoring: false,
+			showJobs: false,
+			showTips: false,
+			showProjects: true
+		});
 	},
 	render:function()
 	{
@@ -141,7 +201,8 @@ var HomeDisplay = React.createClass({
 					                        <img src={post.image.url()} />
 					                        
 				                            <br />
-				                            <br />
+				                            <hr />
+				                            <span>{post.summary}</span>
 				                            <br />
 				                            <hr />
 				                            <span>{post.body}</span>
@@ -161,18 +222,49 @@ var AboutDisplay = React.createClass({
 	},
 	render:function()
 	{
-		return <h1>About</h1>;
+		return(<ul>
+                <div style={postDisplayStyle}>
+                	<h1>About</h1>
+                	<hr />
+                	<p>Welcome, the SJSU Computer Science club is available for students to unwind, grab a bite to eat and study.</p>
+                    <br />
+                    <br />
+                    <hr />
+                </div>
+                <div style={postDisplayStyle}>
+	                	<h1>Our Goals</h1>
+	                	<hr />
+	                	<p>We plan on hosting events for students to gain realy world experience, in a variety of ways.</p>
+	                    <ul>
+	                    	<li>Hacker Rank Computer Science related problems.</li>
+	                    	<li>Team oriented student projects.</li>
+	                    	<li>SJSU Algorithms practice group.</li>
+	                    </ul>
+	                    <hr />
+	                    
+	            </div>
+                <div style={postDisplayStyle}>
+	                	<h1>Join</h1>
+	                	<hr />
+	                	<p>Joining is easy, just speak to an officer and best of all its free!</p>
+	                	<hr />
+	                    
+	            </div>
+                <div style={postDisplayStyle}>
+                	<h1>Rules</h1>
+                	<hr />
+                	<ul>
+                		<li>No throwing objects.</li>
+                		<li>No form of abuse.</li>
+                		<li>If open seat capacity is less than 5 seats, no GAMING excpect on safa areas.</li>
+                	</ul>
+                	<hr />
+                    
+                </div>
+                </ul>);
 	}
 });
-var JoinDisplay = React.createClass({
-	getInitialState: function() {
-		return { show: false };
-	},
-	render:function()
-	{
-		return <h1>Join</h1>;
-	}
-});
+
 var TutoringDisplay = React.createClass({
 	
 	getInitialState: function() {
@@ -182,6 +274,19 @@ var TutoringDisplay = React.createClass({
 	{
 		return (
 			<ul>
+			<div style={postDisplayStyle}>
+
+            	<h1>Tutoring</h1>
+                <h3>Tutoring hours: </h3>
+                
+                <br />
+                <hr />
+                <span>Tutoring is FREE for students!</span>
+                <br />
+                <hr />
+                <span>Now Hiring!!! Talk to Matthew Pleva and send your resume to matthew.pleva@sjsu.edu</span>
+                <hr />
+            </div>
                         {this.props.tutors.map(function(tutor) 
                         	{
                         		
@@ -204,6 +309,17 @@ var JobsDisplay = React.createClass({
 	{
 		return (
 			<ul>
+				<div style={postDisplayStyle}>
+                	<h1>Job Postings</h1>
+                    <h3>All postings are paid in USA $.</h3>
+                    
+                    <br />
+                    <hr />
+                    <span>If you would you like to announce a job posting please contact a club officer</span>
+                    <br />
+                    
+                    <hr />
+                </div>
                         {this.props.posts.map(function(post) 
                         	{
                         		if(post.isJob == true )
@@ -211,9 +327,17 @@ var JobsDisplay = React.createClass({
 			                        return(
 				                        <div style={postDisplayStyle}>
 
+				                        	<li>{post.title}</li>
 					                        <img src={post.image.url()} />
-				                                <li>{post.title}</li>
-			                                </div>);
+				                            
+				                            <br />
+				                            <hr />
+				                            <span>{post.summary}</span>
+				                            <br />
+				                            <hr />
+				                            <span>{post.body}</span>
+				                            <hr />
+			                            </div>);
 	                    		}
                         })}
 			</ul>
@@ -228,6 +352,15 @@ var TipsDisplay = React.createClass({
 	{
 		return (
 			<ul>
+			<div style={postDisplayStyle}>
+
+            	<h1>Useful Info</h1>
+                <hr />
+                <span>Here is some useful information we thought students might enjoy.</span>
+                <br />
+                <hr />
+        
+            </div>
                         {this.props.posts.map(function(post) 
                         	{
                         		if(post.isUsefulPost == true )
@@ -235,9 +368,16 @@ var TipsDisplay = React.createClass({
 			                        return(
 				                        <div style={postDisplayStyle}>
 				                        	
+				                        	<li>{post.title}</li>
 					                        <img src={post.image.url()} />
-				                                <li>{post.title}</li>
-			                                </div>);
+				                            <br />
+				                            <hr />
+				                            <span>{post.summary}</span>
+				                            <br />
+				                            <hr />
+				                            <span>{post.body}</span>
+				                            <hr />
+			                            </div>);
 	                    		}
                         })}
 			</ul>
@@ -252,6 +392,16 @@ var ProjectsDisplay = React.createClass({
 	{
 		return (
 			<ul>
+			<div style={postDisplayStyle}>
+
+            	<h1>Ongoing Student Projects</h1>
+                <hr />
+                <span>These are student oriented projects meant to be open source, coding experience and networking.</span>
+                <br />
+                <hr />
+        
+            </div>
+
                         {this.props.posts.map(function(post) 
                         	{
                         		if(post.isProject == true )
